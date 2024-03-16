54af054dfcfdb6bcf5a70cdb778b4fe3c563e0e9
Eng_Mosad <mohammedmosaad869@gmail.com>
2024-03-16 22:19:01 +0200

    add images to the the projects

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class Dalel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
