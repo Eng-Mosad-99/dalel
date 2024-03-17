78c71a7a5f8e09f275952d4f0f4fb1e9048d07b9
Eng_Mosad <mohammedmosaad869@gmail.com>
2024-03-17 23:15:11 +0200

    craete splash view

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,11 @@
+abstract class AppStrings {
+  static const String splashText = 'Dalel';
+  static const String nextText = 'Next';
+  static const String skipText = 'Skip';
+  static const String createText = 'Create Account';
+  static const String loginText = 'Login Now';
+  static const String signUpText = 'Sign Up';
+  static const String signInText = 'Sign In';
+  static const String notHaveAccountText = 'Don\'t have account?';
+  static const String forgetPasswordText = 'Forget Password?';
+}
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,40 @@
+import 'package:dalel/core/utils/app_strings.dart';
 import 'package:flutter/material.dart';
+import '../../../../core/functions/navigation.dart';
+import '../../../../core/utils/app_text_styles.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    delayedNavigate(context);
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          AppStrings.splashText,
+          style: AppTextStyle.pacifico400Style64,
+        ),
+      ),
+    );
+  }
+}
+
+void delayedNavigate(context) {
+  Future.delayed(
+    const Duration(seconds: 2),
+    () {
+      customNavigate(context, '/onBoarding');
+    },
+  );
 }
